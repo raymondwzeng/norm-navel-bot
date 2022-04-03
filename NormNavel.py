3cf58a7c69305bb5776db90d21b18ff503a40baa
Richard Tobing <rlumb001@ucr.com>
2022-04-03 00:36:55 -0700

    created correct regex-validated names for the options of each commands (option names need to be lowercase and have no spaces)

--- NormNavel.py
+++ NormNavel.py
@@ -1,6 +1,5 @@
 # NormNavel.py
 import os
-
 import discord
 from discord import Client, Intents, Embed
 from dotenv import load_dotenv
@@ -65,34 +64,114 @@ async def test(ctx):
 async def test(ctx):
     await ctx.send(content="/deleterole str:roleName")
 
-@commands.has_permissions(administrator=True)
-@slash.slash(name="editrole", description="Change the name of an existing role.", guild_ids=guild_ids)
-async def test(ctx):
-    await ctx.send(content="/deleterole str:roleName")
 
-@slash.slash(name="roles", description="View the list of roles.", guild_ids=guild_ids)
-async def test(ctx):
+
+@slash.slash(name="roles", description="View the list of roles.", guild_ids=guild_ids,)
+async def roles(ctx):
     await ctx.send(content="/roles optional: str:category")
 
-@slash.slash(name="assignrole", description="Assign yourself a role.", guild_ids=guild_ids)
-async def test(ctx):
+# -----------------------------------------------------------------------------------------------------
+@commands.has_permissions(administrator=True)
+@slash.slash(
+    name="editrole", 
+    description="Change the name of an existing role.", 
+    guild_ids=guild_ids,
+    options=[
+        create_option(
+            name="role_to_be_edited",
+            description="Choose the Role you want to edit",
+            required=True,
+            option_type=3, 
+        )    
+    ]
+)
+async def edit_role(ctx):
+    await ctx.send(content="/editrole str:oldRoleName str:newRoleName")
+
+
+
+
+@slash.slash(name="assignrole", description="Assign yourself a role.", guild_ids=guild_ids,
+    options=[
+        create_option(
+            name="role_to_be_assigned",
+            description="Choose a role to assign yourself!",
+            required=True,
+            option_type=3, 
+        )
+    ]
+)
+async def assign_role(ctx):
     await ctx.send(content="/assignrole str:role")
 
-@slash.slash(name="unassignrole", description="Unassign yourself a role.", guild_ids=guild_ids)
-async def test(ctx):
+
+
+
+@slash.slash(name="unassignrole", description="Unassign yourself a role.", guild_ids=guild_ids,
+    options=[
+        create_option(
+            name="role_to_be_unassigned ",
+            description="Choose one of your roles that you want to unnassign",
+            required=True,
+            option_type=3, 
+        )
+    ]
+)
+async def unassign_role(ctx):
     await ctx.send(content="/unassignrole str:role")
 
-@slash.slash(name="linkserver", description="link a server to a class role", guild_ids=guild_ids)
-async def test(ctx):
+
+
+
+@slash.slash(name="linkserver", description="link a server to a class role", guild_ids=guild_ids,
+    options=[
+        create_option(
+            name="url_to_be_linked",
+            description="Choose a server to link to your class role",
+            required=True,
+            option_type=3, 
+        )
+    ]
+)
+async def link_server(ctx):
     await ctx.send(content="/linkserver str:role")
 
-@slash.slash(name="unlinkserver", description="unlink all servers from a single class role", guild_ids=guild_ids)
-async def test(ctx):
+
+
+
+@slash.slash(name="unlinkserver", description="unlink all servers from a single class role", guild_ids=guild_ids,
+    options=[
+        create_option(
+            name="role_to_unlink",
+            description="Choose a role for which you want to unlink all servers",
+            required=True,
+            option_type=3, 
+        )
+    ]
+)
+async def unlink_server(ctx):
     await ctx.send(content="/unlinkserver str:role")
 
+
+
+
 @commands.has_permissions(administrator=True)
-@slash.slash(name="nukeservers", description="unlink all servers from all class role", guild_ids=guild_ids)
-async def test(ctx):
+@slash.slash(name="nukeservers", description="unlink all servers from all class role", guild_ids=guild_ids,
+    options=[
+        create_option(
+            name="nuclear_launch_code",
+            description="Enter Nuclear Launch code to nuke server",
+            required=True,
+            option_type=3, 
+        )
+    ]
+)
+async def nuke_server(ctx):
     await ctx.send(content="/nukeservers")
+
+
+
+
+
 
 client.run(TOKEN)
